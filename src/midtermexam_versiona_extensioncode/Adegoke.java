74abb5a0f001fcc966aa118ac8de0dbcf25c5302
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2023-06-23 13:19:45 -0400

    my class is created

---
+++ src/midtermexam_versiona_extensioncode/Adegoke.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author HP
+ */
+public class Adegoke {
+    //This is my Class :)
+    //This is fun :)    
+}
